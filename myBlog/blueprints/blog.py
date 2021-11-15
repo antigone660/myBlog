cbe49ad73d365faac0157670d2f9b67d4a9e4449
pyy <2470944472@qq.com>
2021-11-16 00:00:12 +0800

    I can make comments on an article now

--- myBlog/blueprints/blog.py
+++ myBlog/blueprints/blog.py
@@ -1,7 +1,10 @@
 from operator import pos
 import re
-from flask import Blueprint,render_template,url_for
+from flask import Blueprint,render_template,url_for,flash
+from wtforms.validators import Email
+from myBlog.extensions import db
 from myBlog.modles import Post,Comment
+from myBlog.forms import CommentForm
 blog_bp = Blueprint('blog',__name__)
 
 @blog_bp.route('/',methods=["GET"])
@@ -9,10 +12,24 @@ def index():
     posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template("blog/index.html",posts=posts)
 
-@blog_bp.route('/post/<int:post_id>')
+@blog_bp.route('/post/<int:post_id>',methods=["GET","POST"])
 def show_post(post_id):
     post = Post.query.get_or_404(post_id)
     comments = Comment.query.filter(Comment.post_id==post_id)
-    return render_template("blog/post.html",post=post,comments=comments)
+    form = CommentForm()
+    if form.validate_on_submit():
+        author=form.author.data
+        email=form.email.data
+        body=form.body.data
+        comment=Comment(
+            author=author,
+            email=email,
+            body=body,
+            post=post
+        )
+        db.session.add(comment)
+        db.session.commit()
+        flash(" u publish a comment")
+    return render_template("blog/post.html",post=post,comments=comments,form=form)
 
 
--- myBlog/forms.py
+++ myBlog/forms.py
@@ -3,8 +3,22 @@ from wtforms import StringField, SubmitField, SelectField, TextAreaField, Valida
     BooleanField, PasswordField
 from wtforms.validators import DataRequired, Email, Length, Optional, URL
 from wtforms.widgets.core import SubmitInput
+from flask_ckeditor import CKEditorField
 
 class registerForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(2,10)])
     password = PasswordField('Password',validators=[DataRequired(),Length(6,20)])
     submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    author = StringField('Name', validators=[DataRequired(), Length(1, 30)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 254)])
+    # site = StringField('Site', validators=[Optional(), URL(), Length(0, 255)])
+    body = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField()
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(1, 60)])
+    category = SelectField('Category', coerce=int, default=1)
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField()
